a0be81a8e46ac3d679954add46941fda61572ecd
Abhinav <abhinavagarwal1996@gmail.com>
2021-10-08 19:19:41 -0500

    Finish part1 and start part2a

--- part1/main.py
+++ part1/main.py
@@ -1,32 +1,44 @@
-import os
 import torch
-import json
-import copy
-import numpy as np
 from torchvision import datasets, transforms
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-import logging
-import random
+
+from datetime import datetime, date, time, timedelta
 import model as mdl
 device = "cpu"
-torch.set_num_threads(4)
+NUM_THREADS = 4
+torch.set_num_threads(NUM_THREADS)
 
 batch_size = 256 # batch for one node
+print_every_iteration = 20
+
 def train_model(model, train_loader, optimizer, criterion, epoch):
     """
     model (torch.nn.module): The model created to train
-    train_loader (pytorch data loader): Training data loader
+    train_loader (pytorch input loader): Training input loader
     optimizer (optimizer.*): A instance of some sort of optimizer, usually SGD
     criterion (nn.CrossEntropyLoss) : Loss function used to train the network
     epoch (int): Current epoch number
     """
 
-    # remember to exit the train loop at end of the epoch
-    for batch_idx, (data, target) in enumerate(train_loader):
-        # Your code goes here!
-        break
+    model.train()
+    total_loss = 0
+    correct = 0
+
+    for i, (input, target) in enumerate(train_loader):
+        input, target = input.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(input)
+        train_loss = criterion(output, target)
+        train_loss.backward()
+        optimizer.step()
+
+        pred = output.max(1, keepdim=True)[1]
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        total_loss += train_loss.item()
+
+        if i % print_every_iteration == 0:
+            print("loss: ", train_loss.item(), "|acc: ", 100.*correct/len(train_loader.dataset),
+                  "|avgLoss: ", total_loss / (i+1.), "|i: ", i)
 
     return None
 
@@ -35,9 +47,9 @@ def test_model(model, test_loader, criterion):
     test_loss = 0
     correct = 0
     with torch.no_grad():
-        for batch_idx, (data, target) in enumerate(test_loader):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
+        for i, (input, target) in enumerate(test_loader):
+            input, target = input.to(device), target.to(device)
+            output = model(input)
             test_loss += criterion(output, target)
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).sum().item()
@@ -61,7 +73,7 @@ def main():
     transform_test = transforms.Compose([
             transforms.ToTensor(),
             normalize])
-    training_set = datasets.CIFAR10(root="./data", train=True,
+    training_set = datasets.CIFAR10(root="./input", train=True,
                                                 download=True, transform=transform_train)
     train_loader = torch.utils.data.DataLoader(training_set,
                                                     num_workers=2,
@@ -69,7 +81,7 @@ def main():
                                                     sampler=None,
                                                     shuffle=True,
                                                     pin_memory=True)
-    test_set = datasets.CIFAR10(root="./data", train=False,
+    test_set = datasets.CIFAR10(root="./input", train=False,
                                 download=True, transform=transform_test)
 
     test_loader = torch.utils.data.DataLoader(test_set,
